e95449a61e209ff514ec6929089f45bf92c7aff7
jcduran05 <jcduran05@gmail.com>
2016-10-03 11:13:31 -0400

    Initial commit. Project to learn react.

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    return (
+      <p>hello world</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.getElementById('main'));
